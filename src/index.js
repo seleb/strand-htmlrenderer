871c2aa33862ff45e02f8a73d324be982b7cf4cd
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-04-04 01:08:46 -0400

    switched to .strand extension

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import HtmlRenderer from "./html-renderer";
-import script from "./script.txt";
+import script from "./script.strand";
 
 const renderer = new HtmlRenderer(script);
 window.renderer = renderer; // expose renderer globally to make it easier to play around with
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.txt$/,
+				test: /\.strand$/,
 				use: "raw-loader"
 			}
 		]
